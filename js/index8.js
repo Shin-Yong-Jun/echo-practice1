0c9db556b684d7e6a09dc12abb85f058231a6a86
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-02 17:35:44 +0900

    버튼 간소화 자동슬라이드

--- js/index8.js
+++ js/index8.js
@@ -6,14 +6,37 @@ const container = document.querySelector('.container'),
 
   let currentIndex = 0;
 
-    next.addEventListener('click', () => {
-      currentIndex = (currentIndex + 1) % slideImgs.children.length;
-      const translateValue = -100 * currentIndex + '%';
-      slideImgs.style.transform = `translateX(${translateValue})`;
-    })
+  // 이미지 자동 슬라이드
 
-    prev.addEventListener('click', () => {
-      currentIndex = (currentIndex - 1 + slideImgs.children.length) % slideImgs.children.length;
-      const translateValue = -100 * currentIndex + '%';
-      slideImgs.style.transform = `translateX(${translateValue})`;
-    })
+  function autoSlide() {
+    currentIndex = (currentIndex + 1) % slideImgs.children.length;
+    const translateValue = -100 * currentIndex + '%';
+    slideImgs.style.transform = `translateX(${translateValue})`;
+  }
+  
+  container.addEventListener('mouseleave', ()=>{
+    intervalId=setInterval(autoSlide, 3000);
+  });
+  
+  container.addEventListener('mouseenter', ()=>{
+    clearInterval(intervalId);
+  });
+  
+
+  
+  // 이전 다음 버튼 클릭시 이미지 이동
+  next.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slideImgs.children.length;
+    const translateValue = -100 * currentIndex + '%';
+    slideImgs.style.transform = `translateX(${translateValue})`;
+  })
+  
+  prev.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slideImgs.children.length) % slideImgs.children.length;
+    const translateValue = -100 * currentIndex + '%';
+    slideImgs.style.transform = `translateX(${translateValue})`;
+  })
+  
+  // 처음부터 자동슬라이드 작동
+  intervalId=setInterval(autoSlide, 3000);
+  autoSlide();
